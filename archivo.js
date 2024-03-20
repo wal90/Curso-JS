57c9924cd12247559a7dcc621453ebadc53a0c4b
wal90 <walteromartinez90@gmail.com>
2024-03-19 22:11:15 -0300

    valor 1

--- archivo.js
+++ archivo.js
@@ -1,9 +1,18 @@
 // VARIABLES --> almacenan datos
 
-var nombre = 'Walter'; // VAR ---> de modo global
+/*var nombre = 'Walter'; // VAR ---> de modo global
 var Nombre = 'Omar'
 
 let saludo = 'Hola' // LET ---> de modo local
 
-document.write(nombre)
+document.write(nombre)*/
+
+
+// OPERADORES DE ASIGNACION DECREMENTO E INCREMENTO
+
+let a = 1;
+
+document.write('Valor de a: ', a);
+document.write('<br>');
+
 
