c5081f06be9725c853c37b0211dd7f7eb1c5afe8
wal90 <walteromartinez90@gmail.com>
2024-03-23 19:23:46 -0300

    elementos

--- arrays.js
+++ arrays.js
@@ -1,2 +1,9 @@
-let numeros = [];
-numeros = [15,80,650,50.30,-10]
+// Declaracion de un array
+let numeros = [15,80,650,50.30,-10];
+document.write('Elementos: ', numeros);
+document.write('<br>');
+
+document.write('Primer elemento: ', numeros[0]); // 15
+document.write('<br>');
+numeros[0] = 14;
+document.write('Elementos: ', numeros); //---> ahora el primer elemento es el 14
