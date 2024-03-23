35b9959339eb2881cc36f59ff5558c86ff4ce10c
wal90 <walteromartinez90@gmail.com>
2024-03-23 19:38:24 -0300

    to string

--- arrays.js
+++ arrays.js
@@ -11,4 +11,22 @@ document.write('<br>-----------------');
 
 let frutas = ['manzanas', 'peras', 'naranjas', 'mangos'];
 document.write('<br>');
-document.write('Frutas: ', frutas)
+document.write('Frutas: ', frutas);
+document.write('<br>');
+
+// METODOS DE LOS ARRAYS
+
+// VER ACNTIDAD DE ELEMENTOS
+document.write('Cantidad: ', frutas.length); //4
+document.write('<br>');
+
+// SABER ÚLTIMO ELEMENTOS
+document.write('Último elemento: ', numeros[numeros.length-1]); // -10
+document.write('<br>');
+
+// ARRAYS EN TIPO TEXTO
+document.write('En string: ', numeros.toString()); // ahora son de tipo texto
+document.write('<br>');
+
+// UNIR TIPOS DE ARRAYS
+
