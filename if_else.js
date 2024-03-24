584501d80f4ef5c0cbc2e8d05763149c82cdf7df
wal90 <walteromartinez90@gmail.com>
2024-03-24 13:42:20 -0300

    pase int edad

--- if_else.js
+++ if_else.js
@@ -1,7 +1,16 @@
 let nombre, edad;
 nombre = prompt('INGESE SU NOMBRE: ');
 edad = prompt('ingese su edad: ');
+edad = parseInt(edad); // Lo parseo para que si o si tome numero entero no otro tipo de dato
 
 if (edad >= 18) {
     document.write('BIENVENIDO!!', nombre)
+} else {
+    if (edad < 18) {
+        document.write(nombre, 'Eres menor de edad');
+    }
+    else{
+        document.write('NO HAS INGRESADO DATOS');
+    }
+   
 }
