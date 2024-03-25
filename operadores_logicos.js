0f5fd762960395f99c932b16cb6c9ad48c60bfda
wal90 <walteromartinez90@gmail.com>
2024-03-25 19:44:01 -0300

    or

--- operadores_logicos.js
+++ operadores_logicos.js
@@ -11,4 +11,20 @@ edad = prompt('ingrese su edad: ');
 
 if (continente == 'America' && edad >= 18){
     document.write('Eres un adulto americano')
+} else {
+    document.write('O  no eres adulto, o no eres americano')
 }
+
+// OPERADOR || (OR)
+
+let dia, mes, anio; // No se puede poner una variable con Ñ
+dia = prompt('INGRESE DIA: ');
+mes = prompt('INGRESE MES: ');
+anio = prompt('INGRESE AÑO');
+
+if ( mes == 1 || mes == 2 || mes == 3){
+    document.write('Pertenese al primer trimestre!');
+} else {
+    document.write('No pertenece al primer trimestre')
+}
+
