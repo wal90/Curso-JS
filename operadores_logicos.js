68554f0d580bbc6f3ac39dd2cdf5aaf1645f911d
wal90 <walteromartinez90@gmail.com>
2024-03-25 19:34:26 -0300

    condicion continente

--- operadores_logicos.js
+++ operadores_logicos.js
@@ -3,3 +3,12 @@ let a, b;
 a = 30;
 b = 30;
 document.write(a == b); // true 
+
+
+let continente, edad;
+continente = prompt('INGRESE SU CONTINENTE: ');
+edad = prompt('ingrese su edad: ');
+
+if (continente == 'America' && edad >= 18){
+    document.write('Eres un adulto americano')
+}
