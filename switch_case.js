0dc13b0c2312ba3fb339f4acb2c9820d7913f1c5
wal90 <walteromartinez90@gmail.com>
2024-03-26 19:10:30 -0300

    1 2 3

--- switch_case.js
+++ switch_case.js
@@ -1,2 +1,18 @@
 let valor;
-valor = paserInt(prompt('Ingrese un valor entre 1 y 3'))
+valor = paserInt(prompt('Ingrese un valor entre 1 y 3'));
+
+
+switch (valor) {
+    case 1:
+        document.write('Ingreso 1');
+        break;
+    case 2:
+            document.write('Ingreso 2');
+        break;
+    case 3:
+            document.write('Ingreso 3');
+        break;
+
+    default:
+        break;
+}
