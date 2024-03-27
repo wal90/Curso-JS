774b3e4283a0acb588764cca9e51947d6daef9c7
wal90 <walteromartinez90@gmail.com>
2024-03-27 13:29:05 -0300

    suma valor

--- while_1.js
+++ while_1.js
@@ -1,8 +1,19 @@
-let f = 1;
+// let f = 1;
 
-while (f <=10) {
-    document.write('Vuelta no. ', f);
-    document.write('<br>');
+// while (f <=10) {
+//     document.write('Vuelta no. ', f);
+//     document.write('<br>');
+//     f++;
+// }
+// document.write('FIN DEL BUCLE')
+
+let f = 1;
+let suma = 0;
+let valor;
+
+while (f <= 5) { // PARA QUE SOLO LO HAGA 5 VECES
+    valor = parseInt(prompt('Ingrese valor'))
+    suma = suma + valor;
     f++;
 }
-document.write('FIN DEL BUCLE')
+document.write('La suma es: ', suma, '<br>')
