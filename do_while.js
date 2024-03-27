57b3a3f9005c08cf54fb1636dade6f13e6c736b6
wal90 <walteromartinez90@gmail.com>
2024-03-27 18:50:50 -0300

    while valor != 1

--- do_while.js
+++ do_while.js
@@ -1,3 +1,7 @@
+let valor;
 do {
-    
-} while (condition);
+    valor = parseInt(prompt('Ingrese un valor 1 para salir..'));
+    document.write('Ingreso valor: ', valor)
+    document.write('<br>')
+
+} while (valor !== 1); // Mientras que haga esto.. que haga lo de arriba
