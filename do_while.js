c9b18756392f1de964eaa990c9ec8866f77ce27b
wal90 <walteromartinez90@gmail.com>
2024-03-27 19:15:14 -0300

    usuario, clave, control

--- do_while.js
+++ do_while.js
@@ -1,7 +1,16 @@
-let valor;
-do {
-    valor = parseInt(prompt('Ingrese un valor 1 para salir..'));
-    document.write('Ingreso valor: ', valor)
-    document.write('<br>')
+// let valor;
+// do {
+//     valor = parseInt(prompt('Ingrese un valor 1 para salir..'));
+//     document.write('Ingreso valor: ', valor)
+//     document.write('<br>')
 
-} while (valor !== 1); // Mientras que haga esto.. que haga lo de arriba
+// } while (valor !== 1); // Mientras que haga esto.. que haga lo de arriba, cuando se cumple se termina
+// document.write('Fin del bucle')
+
+// Do hace algo.. MIENTRAS se cumpla esta condición
+
+let usuario, clave, control;
+
+control = 0;
+usuario = prompt('Ingrese usuario: ');
+clave = prompt('Ingrese su contraseña: ');
