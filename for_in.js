1d858d223c12bb70991ceceeb010261995a3dd24
wal90 <walteromartinez90@gmail.com>
2024-03-29 00:25:59 -0300

    vocales

--- for_in.js
+++ for_in.js
@@ -9,3 +9,22 @@ for(let f in palabra){
     document. write(f);
     document.write('<br'); // va ir del 0 al 9.. porque la palabra Javascript tiene 10 letras
 }
+
+// for(let f in palabra){
+//     document. write(palabra[f]); // de esta manera me trae cada letra
+//     document.write('<br'); // 
+// }
+
+// ------------------------------------------
+// quiero saber cuantas vocales tiene la palabra ej:
+
+let palabra2 = 'murcielago';
+let vocal = 0; // aca van a ir la cantidad de vocales que encuentro en mi variable
+
+for(let f in palabra2){
+    if (palabra2[f]== 'a' ||palabra2[f]== 'e' ||palabra2[f]== 'i' ||palabra2[f]== 'o' ||palabra2[f]== 'u'){
+        vocal++; // que le sume un valor a la variable valor
+    }
+    
+}
+document.write('Cantidad de vocales: ', vocal)
