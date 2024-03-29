607691aad189acc0505772fc9f46d279787bf1c3
wal90 <walteromartinez90@gmail.com>
2024-03-29 00:03:12 -0300

    f+=2

--- bucle_for.js
+++ bucle_for.js
@@ -5,6 +5,16 @@
 // 2 es la condicion para que el bucle se realice
 // 3 para darle un valor a f... y salga del bucle osea termine
 for (let f= 1; f < 5; f++) {
-    document.write('Vuelta no. ', f)
-    
+    document.write('Vuelta no. ', f);
+    document.write('<br>')  
 }
+document.write('FIN DEL BUCLE!!')
+
+
+// f+=2 ---> siginifica que f vaya de dos en dos empezando desde el 1, entonces: 1,3,5,7,9...
+
+// for (let f= 1; f < 20; f+=2) {
+//     document.write('Vuelta no. ', f);
+//     document.write('<br>')  
+// }
+// document.write('FIN DEL BUCLE!!')
