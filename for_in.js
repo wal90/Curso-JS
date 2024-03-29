47073c7f4be947573922476ddc89a56a350cee16
wal90 <walteromartinez90@gmail.com>
2024-03-29 00:17:35 -0300

    for in explicacion

--- for_in.js
+++ for_in.js
@@ -1,1 +1,11 @@
 // FOR IN ---> utiliza un rango de valores especificos para hacer un bucle
+// SOLO FUNCIONA PARA OBJETOS QUE TIENEN UNA CANTIDAD DE ELEMENTOS, no para valores enteros
+
+let palabra = 'JavaScript';
+ 
+// let f = seria el contador, es la variable de control, esa variable va a realizar la condicion que le vamos a poner
+// en este caso el for in va a dar una vuelta por cada letra de la palabra
+for(let f in palabra){
+    document. write(f);
+    document.write('<br'); // va ir del 0 al 9.. porque la palabra Javascript tiene 10 letras
+}
