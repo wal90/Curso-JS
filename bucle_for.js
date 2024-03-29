e358ae6fca2fffd31753233be6913a6e256cb6f1
wal90 <walteromartinez90@gmail.com>
2024-03-28 23:58:27 -0300

    explicacion estructura

--- bucle_for.js
+++ bucle_for.js
@@ -1,6 +1,9 @@
 // BUCLE FOR se usa cuando sabemos las veces que tiene que iterar
 
-for () {
+
+// 1 desde donde yo quiero que inicie el bucle
+// 2 es la condicion para que el bucle se realice
+for (let f= 1; f < 5;) {
     
     
 }
