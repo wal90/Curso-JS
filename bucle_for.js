fa6aeb6e4a147042efbcd80172d5946b5f02c549
wal90 <walteromartinez90@gmail.com>
2024-03-28 23:59:39 -0300

    estructura terminada

--- bucle_for.js
+++ bucle_for.js
@@ -3,7 +3,8 @@
 
 // 1 desde donde yo quiero que inicie el bucle
 // 2 es la condicion para que el bucle se realice
-for (let f= 1; f < 5;) {
-    
+// 3 para darle un valor a f... y salga del bucle osea termine
+for (let f= 1; f < 5; f++) {
+    document.write('Vuelta no. ', f)
     
 }
