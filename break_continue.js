95ea4fd51997dd5f15e908929e7cd2ba1e0e7c38
wal90 <walteromartinez90@gmail.com>
2024-04-01 19:10:00 -0300

    if break

--- break_continue.js
+++ break_continue.js
@@ -1,4 +1,9 @@
 for(let f=0; f < 10; f++){
     document.write(f);
     document.write('<br>')
+    if (f == 3){
+        document.write('Aqui es la vuelta: ',f);
+        document.write('<br>')
+        break; // se detiene aca
+    }
 }
