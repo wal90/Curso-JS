781469d62f90498bf624a2acba6cbedd45ea143e
wal90 <walteromartinez90@gmail.com>
2024-04-01 19:55:19 -0300

    ej functions

--- funciones_01.js
+++ funciones_01.js
@@ -1,5 +1,19 @@
 // Función declarativa
 function saludar(){
-    document.write('Hola a todos!!');
+    alert('Hola a todos!!'); // alert es una ventana
+}
+
+function despedir(){
+    document.write('Adios a todos!');
 }
 saludar(); // las funciones siempre se invocan
+
+// Dentro de una funcuion se puede invocar a otra e
+
+function saludar2(){
+    document.write('Hola a todos!!'); // alert es una ventana
+    document.write('<br>')
+    saludar();
+}
+
+// Otro ejemplo
