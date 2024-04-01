ebc921ff9b22278ac94603a724bf33cab98e0a66
wal90 <walteromartinez90@gmail.com>
2024-04-01 19:14:57 -0300

    continue a

--- break_continue.js
+++ break_continue.js
@@ -1,4 +1,4 @@
-for(let f=0; f < 10; f++){
+/*for(let f=0; f < 10; f++){
     document.write(f);
     document.write('<br>')
     if (f == 3){
@@ -6,4 +6,18 @@ for(let f=0; f < 10; f++){
         document.write('<br>')
         break; // se detiene aca
     }
+}*/
+
+let palabra = 'Javascript';
+let resultado = '';
+
+for(let f in palabra){
+    if(palabra[f]=='a'){
+        continue; // salta el valor
+    }else {
+        resultado += palabra[f] // le va a asignar a resultado esa letra, que no sea una a
+    }
 }
+document.write(resultado) // Jvscript
+
+// El resultado es que se salteee todas las a, osea que de la vuelta encuentra una a y continua
