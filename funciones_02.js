8e80a8819d027f633f9c101adb61bb8e4e5306d0
wal90 <walteromartinez90@gmail.com>
2024-04-02 19:47:15 -0300

    suma

---
+++ funciones_02.js
@@ -0,0 +1,9 @@
+// FUNCIONES CON PARÁMETROS
+// parametros son valores que voy a utilizar
+let resultado;
+function suma(valor1, valor2) {
+    resultado = valor1 + valor2;
+    alert('Resultado: ' + resultado)
+}
+
+suma(5,8)
