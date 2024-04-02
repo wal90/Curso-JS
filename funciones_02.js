4372699497f20e1fd34c973eb6ed28c3df6b33ed
wal90 <walteromartinez90@gmail.com>
2024-04-02 20:04:46 -0300

    pase int

--- funciones_02.js
+++ funciones_02.js
@@ -2,6 +2,8 @@
 // parametros son valores que voy a utilizar
 let resultado;
 function suma(valor1, valor2) {
+    valor1 = parseInt(valor1);
+    valor2 = parseInt(valor2);
     resultado = valor1 + valor2;
     alert('Resultado: ' + resultado)
 }
