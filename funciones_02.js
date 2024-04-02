8a9cc87fb18b2db878286f925e3d910925c688a2
wal90 <walteromartinez90@gmail.com>
2024-04-02 20:07:02 -0300

    prompt

--- funciones_02.js
+++ funciones_02.js
@@ -2,10 +2,10 @@
 // parametros son valores que voy a utilizar
 let resultado;
 function suma(valor1, valor2) {
-    valor1 = parseInt(valor1);
-    valor2 = parseInt(valor2);
+    valor1 = parseInt(prompt('INGRESE VALOR 1: '));
+    valor2 = parseInt(prompt('INGRESE VALOR 2: '));
     resultado = valor1 + valor2;
     alert('Resultado: ' + resultado)
 }
 
-suma(5,8)
+suma()
