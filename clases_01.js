143272e359022edd5bfaeb202002bcaa6fe53f1a
wal90 <walteromartinez90@gmail.com>
2024-04-04 19:39:21 -0300

    anotaciones

---
+++ clases_01.js
@@ -0,0 +1,13 @@
+// La CLASE es como un molde para trabajar objetos
+// Forma de DEFINIR objetos y estructura de datos con un conjunto de propiedades y métodos
+// Los atributos son caracteristicas de los objetos, ej nombre
+class Persona{
+    nombre = 'Homero';
+    apellido = 'Simpson';
+    direccion = 'Av. Siempreviva 742';
+    telefono = 5553472;
+    email = 'amantedelacomida@aol.com';
+    trabajar(){
+        return 
+    }
+}
