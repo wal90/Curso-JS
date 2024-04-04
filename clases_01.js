f5e9911d4250272b64d84abcb64f64abc1ba2f2c
wal90 <walteromartinez90@gmail.com>
2024-04-04 19:45:26 -0300

    new persona

--- clases_01.js
+++ clases_01.js
@@ -1,6 +1,8 @@
 // La CLASE es como un molde para trabajar objetos
 // Forma de DEFINIR objetos y estructura de datos con un conjunto de propiedades y métodos
 // Los atributos son caracteristicas de los objetos, ej nombre
+
+
 class Persona{
     nombre = 'Homero';
     apellido = 'Simpson';
@@ -8,6 +10,12 @@ class Persona{
     telefono = 5553472;
     email = 'amantedelacomida@aol.com';
     trabajar(){
-        return 
+        return 'Trabaja en la planta nuclear';
+    }
+    estudiar(){
+        return 'Escuela primaria de Sprinfield'
     }
 }
+
+// Aca estamos definiendo un objeto, que tiene los atributos y métodos de Persona
+const homero = new Persona()
