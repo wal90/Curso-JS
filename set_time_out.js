a51db858867a2fe20fed408eb771771b2dca9646
wal90 <walteromartinez90@gmail.com>
2024-04-15 21:52:41 -0300

    tiempo cumplido

--- set_time_out.js
+++ set_time_out.js
@@ -1,3 +1,8 @@
 function comenzarTiempo(){
-    
+    // set time out es una funcion que ejecuta una funcion (tiempo cumplido) luego de un determinado tiempo asigando (5000, osea 5 segundo)
+    setTimeout(tiempocumplido, 5000); // conteo en milisegundos
+}
+
+function tiempocumplido(){
+    alert('Se termino el tiempo')
 }
