af4347fff6b0fcd5da6692a8aa3eadbeadc5dda0
wal90 <walteromartinez90@gmail.com>
2024-04-15 21:57:43 -0300

    tiempo elegido

--- set_time_out.js
+++ set_time_out.js
@@ -1,6 +1,8 @@
+let elementoSegundos = document.getElementById('tiempoElegido').value; // siempre que hay un get elemnt by id hay que poner un value... asi toma el valor
+
 function comenzarTiempo(){
     // set time out es una funcion que ejecuta una funcion (tiempo cumplido) luego de un determinado tiempo asigando (5000, osea 5 segundo)
-    setTimeout(tiempocumplido, 5000); // conteo en milisegundos
+    setTimeout(tiempocumplido, 1000*elementoSegundos); // conteo en milisegundos
 }
 
 function tiempocumplido(){
