8d857e44f0f28491236220390e6db908f1d2650e
wal90 <walteromartinez90@gmail.com>
2024-04-15 22:05:39 -0300

    elemento texto alarma

--- set_time_out.js
+++ set_time_out.js
@@ -1,10 +1,16 @@
-let elementoSegundos = document.getElementById('tiempoElegido').value; // siempre que hay un get elemnt by id hay que poner un value... asi toma el valor
-
+let elementoSegundos = document.getElementById('tiempoElegido').value; // siempre que hay un get elemnt by id hay que poner un value, ESTO SOLO CUANDO TIENE QUE TOMAR LOS VALORES INGRESADOS POR EL USUARIO... asi toma el valor
+let elementoTextoAlarma = document.getElementById('textoAlarma');
 function comenzarTiempo(){
     // set time out es una funcion que ejecuta una funcion (tiempo cumplido) luego de un determinado tiempo asigando (5000, osea 5 segundo)
     setTimeout(tiempocumplido, 1000*elementoSegundos); // conteo en milisegundos
 }
 
+
+// function tiempocumplido(){
+//     alert('Se termino el tiempo')
+// }
+
 function tiempocumplido(){
-    alert('Se termino el tiempo')
+    elementoTextoAlarma.textContent = "ENCENDIDO";// textContent: es para que lo seleccionado muestre el texto
+    elementoTextoAlarma.style.color = "green";
 }
