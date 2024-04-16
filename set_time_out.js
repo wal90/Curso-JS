01648e534e17ee2480009d60f4fa2153f681b8ab
wal90 <walteromartinez90@gmail.com>
2024-04-16 20:58:23 -0300

    audio alarma

--- set_time_out.js
+++ set_time_out.js
@@ -1,5 +1,6 @@
 let elementoSegundos = document.getElementById('tiempoElegido').value; // siempre que hay un get elemnt by id hay que poner un value, ESTO SOLO CUANDO TIENE QUE TOMAR LOS VALORES INGRESADOS POR EL USUARIO... asi toma el valor
 let elementoTextoAlarma = document.getElementById('textoAlarma');
+let elementoSonidoAlarma = document.getElementById('audioAlarma')
 function comenzarTiempo(){
     // set time out es una funcion que ejecuta una funcion (tiempo cumplido) luego de un determinado tiempo asigando (5000, osea 5 segundo)
     setTimeout(tiempocumplido, 1000*elementoSegundos); // conteo en milisegundos
