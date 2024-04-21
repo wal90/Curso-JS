bf5b59f1b6cf1aac5e829a3e59e2e103fbf0e6e8
wal90 <walteromartinez90@gmail.com>
2024-04-21 19:20:39 -0300

    anonimas y nombradas

--- Clases_02_definicion.js
+++ Clases_02_definicion.js
@@ -4,3 +4,19 @@ class Papel{ // clase Papel que sirve para crear nuevos papeles
         this.ancho = ancho;
     }
 }
+
+let mipapel = new Papel(4,5) // obj Papel a traves de una instancia de Papel
+
+let Papel_A = class{ // Clase anonima porque no tiene un nombre la clase
+    constructor(alto, ancho){ 
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+}
+
+let Papel_B = class Papelx{ 
+    constructor(alto, ancho){ 
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+}
