4e89306f1bfca12feebd7d224387a0399577257e
wal90 <walteromartinez90@gmail.com>
2024-04-21 19:13:54 -0300

    anotaciones

--- Clases_02_definicion.js
+++ Clases_02_definicion.js
@@ -1,5 +1,5 @@
-class Papel{
-    constructor(alto, ancho){
+class Papel{ // clase Papel que sirve para crear nuevos papeles
+    constructor(alto, ancho){ // es un constructor dentro de un obj de una clase
         this.alto = alto;
         this.ancho = ancho;
     }
