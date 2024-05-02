8b7c82152ff314d91ac465ddda8d204a2fc10297
wal90 <walteromartinez90@gmail.com>
2024-05-02 00:08:16 -0300

    direccion

--- Club los Magios/miscrips.js
+++ Club los Magios/miscrips.js
@@ -6,6 +6,18 @@ function consulta(){
     fetch("resumen.json")
     .then(respuesta => respuesta.json())
     .then((salida)=>{datosjson = salida;
+
+        document.getElementById("titular")
+        titular.textContent = salida.titular;
+
+        document.getElementById("no_miembro")
+        no_miembro.textContent = salida.no_miembro;
+
+        document.getElementById("direccion")
+        direccion.textContent = salida.direccion;
+
+
+
     
     })
 
