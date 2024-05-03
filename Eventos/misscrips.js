2350f456356922b7dc6283d9857a4d8ea6cc5ee6
wal90 <walteromartinez90@gmail.com>
2024-05-03 19:44:19 -0300

    mouse over

--- Eventos/misscrips.js
+++ Eventos/misscrips.js
@@ -4,8 +4,13 @@ function muestramensaje(){
     alert("Boton presionado!")
 }
 
+function mensaje2(){
+    alert("Soy un mouseover!")
+}
 
-boton.addEventListener("click", muestramensaje)
+boton.addEventListener("mouseover", mensaje2);
+
+boton.addEventListener("click", muestramensaje);
 
 // boton.addEventListener("click", function(){
 //     alert("Presionaste el boton");
