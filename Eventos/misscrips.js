27e87cc932113cdf387536baaf9d081a2bc8e5c6
wal90 <walteromartinez90@gmail.com>
2024-05-05 22:50:38 -0300

    keypress

--- Eventos/misscrips.js
+++ Eventos/misscrips.js
@@ -59,7 +59,10 @@ function verificaNumero(event){
 texto1.addEventListener('keydown', verificaNumero); // keydown --> es si el usuario ejecuta cualquier techa del teclado
 
 texto1.addEventListener('keyup', function(event){
-    console.log('Entrada del usuario: ' + event.target.value)
+    console.log('Entrada del usuario: ' + event.target.value) // valor del campo, es todo lo que va a estar en el input
 });// keyup --> es cuyando se suelta una tecla
 
 
+texto1.addEventListener('keypress', function(event){
+    console.log('Caracetr ingresado: ' + event.key) // la tecla que se presiono, es lo que se presiona
+} ) // keypress ----> es cuando se presiona y se suelta rapidamente una tecla
