11c5a45a6fedf303c7d42e9d85d119fbf860573e
wal90 <walteromartinez90@gmail.com>
2024-05-09 22:43:10 -0300

    cambios

--- Eventos/misscripts.js
+++ Eventos/misscripts.js
@@ -124,26 +124,3 @@ socket.onmessage = function(event){
 }
 
 
-//----------------- PÁGINA 5 --------------------------------------------------------------------------------------
-
-
-let audio = document.getElementById('audio');
-let listaCanciones = document.getElementById('listaCanciones');
-
-listaCanciones.addEventListener('change', cambiarCancion);
-
-function cambiarCancion(){
-    let cancionElegida = listaCanciones.value;
-    audio.src = cancionElegida;
-    audio.play();
-
-    let evento = new CustomEvent('CambioDeCancion'); // CustomEvent ---> es un evento personalizado
-    // hay q definir cuando se ejecuta mi evento
-    audio.dispatchEvent(evento);
-}
-
-audio.addEventListener('CambioDeCancion', mostrarCancion);
-
-function mostrarCancion(){
-    console.log('Canción actual: ' + listaCanciones.value);
-}
