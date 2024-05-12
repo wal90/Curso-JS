baf6d3c576ec9bfa05d06d44f4a47d480973c5c0
wal90 <walteromartinez90@gmail.com>
2024-05-12 12:31:20 -0300

    get element by id

---
+++ Eventos/Buscador Peliculas/misscripts.js
@@ -0,0 +1,4 @@
+let selector = document.getElementById('miSelector');
+let input = document.getElementById('miInput');
+let boton = document.getElementById('miBoton');
+let listado = document.getElementById('miListado');
