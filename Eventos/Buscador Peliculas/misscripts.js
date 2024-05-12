5dc5ae0ec74ca4f93d4688dd8fc4fe801fe532cc
wal90 <walteromartinez90@gmail.com>
2024-05-12 12:36:50 -0300

    sweet alert

--- Eventos/Buscador Peliculas/misscripts.js
+++ Eventos/Buscador Peliculas/misscripts.js
@@ -2,3 +2,11 @@ let selector = document.getElementById('miSelector');
 let input = document.getElementById('miInput');
 let boton = document.getElementById('miBoton');
 let listado = document.getElementById('miListado');
+
+let archivo = 'peliculas.json';
+
+selector.addEventListener('change', cambiarArchivo);
+
+function cambiarArchivo(){
+    archivo = selector.value;
+}
