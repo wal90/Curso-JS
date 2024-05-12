e93443928523e9d7ed78ca0921a6cd7852557f55
wal90 <walteromartinez90@gmail.com>
2024-05-12 12:47:14 -0300

    swal selector

--- Eventos/Buscador Peliculas/misscripts.js
+++ Eventos/Buscador Peliculas/misscripts.js
@@ -6,7 +6,14 @@ let listado = document.getElementById('miListado');
 let archivo = 'peliculas.json';
 
 selector.addEventListener('change', cambiarArchivo);
+selector.addEventListener('Cambiomodo', mensajemodo)
 
 function cambiarArchivo(){
     archivo = selector.value;
+    let evento = new CustomEvent('Cambiomodo')
+
+}
+
+function mensajemodo(){
+    swal.fire('El archivo de busqueda ahora es ' + selector.value);
 }
