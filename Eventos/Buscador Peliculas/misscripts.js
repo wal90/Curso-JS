14c9aa10d2e0ff6e56beafeba4e1fd5801be0c6d
wal90 <walteromartinez90@gmail.com>
2024-05-13 19:50:32 -0300

    buscar

--- Eventos/Buscador Peliculas/misscripts.js
+++ Eventos/Buscador Peliculas/misscripts.js
@@ -6,8 +6,9 @@ let listado = document.getElementById('miListado');
 let archivo = 'peliculas.json';
 
 selector.addEventListener('change', cambiarArchivo);
-selector.addEventListener('Cambiomodo', mensajemodo)
-input.addEventListener('keydown', verificaInput)
+selector.addEventListener('Cambiomodo', mensajemodo);
+input.addEventListener('keydown', verificaInput);
+boton.addEventListener('click', buscar)
 
 function cambiarArchivo(){
     archivo = selector.value;
@@ -23,4 +24,19 @@ function verificaInput(evento){
     if((evento.keycode <65 || evento.keycode > 90) && evento.keycode != 32 && evento.keycode != 8){
             evento.preventDefault()
     }
+}
+
+function buscar(){
+    listado.innetHTML = ''; // para que se limpe el input con la busqueda anterior
+    fetch(archivo)
+        .then(respuesta => respuesta.json)
+        .then(function(salida){
+            for(let item of salida.data){
+                if(item.nombre.startsWhith(input.value.toUpperCase())){
+                    let p = document.createElement('p');
+                    p.id = item.nombre;
+                    p.innetHTML = item.sinopsis;
+                }
+            }
+        })
 }
