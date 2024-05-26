408bf922ccf5602a1592d491a5ada8c20b4b4afc
wal90 <walteromartinez90@gmail.com>
2024-05-26 19:39:24 -0300

    fetch

--- POST/misscripts.js
+++ POST/misscripts.js
@@ -1,6 +1,10 @@
 async function crearPost(titulo, contenido){
     try {
-        
+        let respuesta = await fetch('https://jsonplaceholder.typicode.com.post', { // obj ini
+            metodo; ,
+            header: ,
+            body:
+        })
     } catch (error) {
         console.error('Algo salio mal al crear el registro:'), error
     }
