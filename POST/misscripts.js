4698b3d8096d2c3b33b79f97cfee5b3811212ec3
wal90 <walteromartinez90@gmail.com>
2024-05-26 19:55:51 -0300

    valores

--- POST/misscripts.js
+++ POST/misscripts.js
@@ -1,11 +1,15 @@
 async function crearPost(titulo, contenido){
     try {
-        let respuesta = await fetch('https://jsonplaceholder.typicode.com.post', { // obj ini
+        let respuesta = await fetch('https://jsonplaceholder.typicode.com.posts', { // obj ini
             metodo: "POST" ,
             header: {
                 "Content-Type": "application/json"
             } , // QUE TIPO DE FORMATO ESTAMOS ENVIANDO EL EN CUERPO DE LA SOLICITUD
-            body: JSON.stringify()// funcion de javascrip que convierte un bjt de javascript en una cadena json
+            body: JSON.stringify({
+                title: titulo,
+                body: contenido,
+                userID: 1,
+            })// funcion de javascrip que convierte un bjt de javascript en una cadena json
         })
     } catch (error) {
         console.error('Algo salio mal al crear el registro:'), error
