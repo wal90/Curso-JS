040f08209859d5616c6c85c926022c11ccce85e7
wal90 <walteromartinez90@gmail.com>
2024-06-01 19:37:50 -0300

    post

--- AXIOS/misscripts2.js
+++ AXIOS/misscripts2.js
@@ -1,2 +1,10 @@
 // FORMA BASICA DE AXIOS
-axios.get().then().catch()
+//axios.get().then().catch()
+
+let datos = {
+    title: 'Nuevo Post',
+    body: 'Mi contenido'
+}
+axios.post('https://jsonplaceholder.typicode.com/posts')
+    .then(response => console.log(response.data))
+    .catch(error=> console.log(error))
