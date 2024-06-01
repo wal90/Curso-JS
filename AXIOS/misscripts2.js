889c48537b87d06461bbbb9808702e05a7fb32b2
wal90 <walteromartinez90@gmail.com>
2024-06-01 19:47:01 -0300

    all & spread

--- AXIOS/misscripts2.js
+++ AXIOS/misscripts2.js
@@ -5,6 +5,22 @@ let datos = {
     title: 'Nuevo Post',
     body: 'Mi contenido'
 }
+
+let pedido1 = axios.get('https://api.ejemplo.com/data1');
+let pedido2 = axios.get('https://api.ejemplo.com/data2');
+let pedido3 = axios.get('https://api.ejemplo.com/data3');
+
+// all ---> pide todo al mismo tiempo
+axios.all([pedido1, pedido2, pedido3])
+.then(axios.spread((respuesta1, respuesta2, respuesta3)=>{
+            // codigo
+        })) // spread ---> distribuye las respuestas en tres variables distintas
+        .catch(error => {
+            //manejar errores
+        })
+
 axios.post('https://jsonplaceholder.typicode.com/posts', datos) // hay q poner el objeto donde quiero que vayan los datos
     .then(response => console.log('El post fue creado con éxito', response.data))
     .catch(error=> console.log('Error al publicar: ', error))
+
+    // AXIOS permite hacer varias solicitudes http al mismo tiempo
