2e90ee267e55ff03b22e6fc7c62b91bcf6f68701
wal90 <walteromartinez90@gmail.com>
2024-06-01 19:39:55 -0300

    datos

--- AXIOS/misscripts2.js
+++ AXIOS/misscripts2.js
@@ -5,6 +5,6 @@ let datos = {
     title: 'Nuevo Post',
     body: 'Mi contenido'
 }
-axios.post('https://jsonplaceholder.typicode.com/posts')
-    .then(response => console.log(response.data))
-    .catch(error=> console.log(error))
+axios.post('https://jsonplaceholder.typicode.com/posts', datos) // hay q poner el objeto donde quiero que vayan los datos
+    .then(response => console.log('El post fue creado con éxito', response.data))
+    .catch(error=> console.log('Error al publicar: ', error))
